2ad9d0e7326b7ea14a37f92886c840695c93cce1
olzh2102 <olzhas2102@yahoo.com>
2022-11-28 20:11:54 +0100

    test

--- .github/actions/prepare_modified/prepare_modified.sh
+++ .github/actions/prepare_modified/prepare_modified.sh
@@ -17,6 +17,31 @@ IS_COMMON_CHANGED_VAR=$(modified "$COMMON_PATHS")
 IS_TERRAFORM_CHANGED_VAR=$(modified "$TERRAFORM_PATH")
 IS_DB_MIGRATION_CHANGED_VAR=$(modified "$DB_MIGRATION_PATH")
 
-if [ $IS_COMMON_CHANGED_VAR -gt 0 ]; then
-    echo "Common changed"
+if [ $IS_COMMON_CHANGED_VAR -gt 0 ]; 
+then
+    echo "Common files were modified"
+    echo $CONFIG
+    echo "modified_modules=$CONFIG" >> $GITHUB_OUTPUT
+else
+    modified_modules="[]"
+    jq -c .[] <<< $CONFIG | while read module; do
+        path=$(jq -c '.path' <<<$module)
+        num=$(git diff --name-only HEAD^ HEAD -- "$path" | wc -l)
+        newValue=$(jq -c --argjson c "$module" '. += [$c]' <<<$modified_modules)
+        modified_modules=$newValue
+        echo $newValue
+        echo "modified_modules=$newValue" >>$GITHUB_OUTPUT
+done
+fi
+
+if [ $IS_TERRAFORM_CHANGED_VAR -gt 0 ]; 
+then
+    echo "Terraform files were modified"
+    echo "terraform=true" >> $GITHUB_OUTPUT
+fi
+
+if [ $IS_DB_MIGRATION_CHANGED_VAR -gt 0 ]; 
+then
+    echo "Database Migration files were modified"
+    echo "db_migration=true" >> $GITHUB_OUTPUT
 fi
